8bbe0eb0575ce94fe9ae245bfe9d29d39417f027
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-01-20 16:17:23 -0300

    Project created

---
+++ SlaesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SlaesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
